d3e5e9b0e908cd99335aab5b1e237ca244f920e8
mdyoyo <ht9938@qq.com>
2016-04-11 22:20:05 +0800

    addMessage

--- message.js
+++ message.js
@@ -1,7 +1,6 @@
 var PORT = 9529;
 var http = require('http');
 var qs = require('qs');
-
 var TOKEN = 'sspku';
 
 function checkSignature(params,token){
@@ -21,13 +20,27 @@ var server = http.createServer(function(request,response){
     var query = require('url').parse(request.url).query;
     var params = qs.parse(query);
 
-    console.log(params);
-    console.log("token-->",TOKEN);
-    if(checkSignature(params,TOKEN)){
-
+    //console.log(params);
+    //console.log("token-->",TOKEN);
+    if(!checkSignature(params,TOKEN)){
+        //如果签名不对，结束请求并返回
+        response.end('signature fail');
+        return;
+    }
+    if(request.method == "GET"){
+        //如果请求是GET，返回echostr用于通过服务器有效校验
         response.end(params.echostr);
     }else{
-        response.end('signature fail');
+        //否则是微信给开发者服务器的POST请求
+        var postdata = "";
+        request.addListener("data",function(postchunk){
+            postdata += postchunk;
+        });
+        //获取到了POST数据
+        request.addListener("end",function(){
+            console.log(postdata);
+            response.end('success');
+        });
     }
 });
 
